25afe116aa1bea2a5535b6bf7714a822f5303b64
Jamieson Walker <Chaymee@users.noreply.github.com>
2026-04-14 17:46:58 -0500

    Update panel.ts

--- src/panel.ts
+++ src/panel.ts
@@ -117,7 +117,7 @@ export class WorkshopPanel {
         if (!folders || folders.length === 0) break;
         const uri = vscode.Uri.joinPath(folders[0].uri, section.triggerFile);
         try {
-          await vscode.commands.executeCommand("vscode.open", uri, vscode.ViewColumn.Two);
+          await vscode.commands.executeCommand("vscode.open", uri, vscode.ViewColumn.One);
         } catch {
           vscode.window.showWarningMessage(`Could not open ${section.triggerFile}`);
         }
